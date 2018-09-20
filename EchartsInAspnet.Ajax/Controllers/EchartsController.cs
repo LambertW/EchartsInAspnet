d0997ec8206687090dd37e9f26dc42d965d71185
Lambert Wu <plclambertwu@outlook.com>
2018-09-20 23:18:15 +0800

    Init ajax project

---
+++ EchartsInAspnet.Ajax/Controllers/EchartsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EchartsInAspnet.Ajax.Controllers
+{
+    public class EchartsController : Controller
+    {
+        // GET: Echarts
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ EchartsInAspnet.Ajax/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EchartsInAspnet.Ajax.MvcApplication" Language="C#" %>
